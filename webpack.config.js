dc940328fbe17cdb7fc0968a389fc7471c6ed417
luis.amengual <luis.amengual@globant.com>
2021-03-13 18:16:00 -0300

    Creo estructura inicial

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/index.ts'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Neochess components',
+            meta: {
+                viewport: 'width=device-width, initial-scale=1'
+            }
+        })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        open: true,
+        contentBase: './www'
+    },
+    mode: "development"
+};
