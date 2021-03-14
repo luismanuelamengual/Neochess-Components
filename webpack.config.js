ef7a5eb526cf4e04adab12f4c8fdac1d3b591445
luis.amengual <luis.amengual@globant.com>
2021-03-14 11:12:22 -0300

    Creo estructura inicial

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ],
     output: {
         path: __dirname + '/dist',
-        filename: '[name].bundle.js'
+        filename: 'neochess-components.bundle.js'
     },
     module: {
         rules: [
