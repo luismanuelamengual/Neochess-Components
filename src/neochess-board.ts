ad03138a11b80de37382f6f7384ba2970d76efeb
luis.amengual <luis.amengual@globant.com>
2021-03-14 11:18:22 -0300

    Creo estructura inicial

--- src/neochess-board.ts
+++ src/neochess-board.ts
@@ -2,6 +2,16 @@ export class NeochessBoardElement extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+
+        const blueBox = document.createElement('div');
+        blueBox.style.background = 'blue';
+        blueBox.style.position = 'absolute';
+        blueBox.style.top = '0';
+        blueBox.style.left = '0';
+        blueBox.style.width = '100%';
+        blueBox.style.height = '100%';
+
+        this.shadowRoot.appendChild(blueBox);
     }
 }
 
