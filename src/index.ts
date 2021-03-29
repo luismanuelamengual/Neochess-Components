3ab220214c2a105b7a93f6e8cf1c94bcd1e691eb
luis.amengual <luis.amengual@globant.com>
2021-03-29 16:49:30 -0300

    Cambios en implementación

--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './neochess-board/neochess-board';
---
+++ src/neochess-components.ts
@@ -0,0 +1,3 @@
+export * from './neochess-board/neochess-board';
+export * from './neochess-board/neochess-board-theme';
+export * from './neochess-board/neochess-board-pieceset';
